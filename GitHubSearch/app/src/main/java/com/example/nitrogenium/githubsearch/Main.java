dd14ad3e3a9ed18f224992e4161cb680c9060073
VladislavDanil <Vldslvdanilv@rambler.ru>
2015-08-08 19:50:12 +0300

    добавил недостающие файлы

---
+++ GitHubSearch/app/src/main/java/com/example/nitrogenium/githubsearch/Main.java
@@ -0,0 +1,169 @@
+package com.example.nitrogenium.githubsearch;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import fragment.FragmentResultLayout;
+
+/**
+ * the class initializes the main activity
+ *
+ * @author Данилов Владислав
+ */
+public class Main extends ActionBarActivity implements SearchView.OnQueryTextListener {
+    private RecordsDbHelper mDbHelper;
+    public static final String STRING_BUNDLE_INDEX = "string";
+    SearchView mSearchView;
+    /**
+     * ads fragment�layout result for further use in the transition
+     */
+    Fragment mFragmentResult;
+    /**
+     * variable declaration for the transition between fragments FragmentTransaction
+     */
+    FragmentTransaction mTransaction;
+
+    /**
+     * method creates the main active,
+     * loads the initial fragment FragmentStartSearchLayout
+     * implements requests suggestion search view
+     *
+     * @param savedInstanceState if non-null, this fragment is being re-constructed from a previous saved state as given here
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout);
+        if (savedInstanceState == null) {
+            mFragmentResult = new FragmentResultLayout();
+            mTransaction = getSupportFragmentManager().beginTransaction();
+            mTransaction.setCustomAnimations(R.anim.start_app, 0);
+            mTransaction.replace(R.id.fragment, mFragmentResult);
+            mTransaction.addToBackStack(null);
+            mTransaction.commit();
+
+        }
+        mDbHelper = new RecordsDbHelper(this);
+
+        //Получаем Intent
+        Intent intent = getIntent();
+        //Проверяем тип Intent
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            saveTask(query);
+            Bundle stringSearch = new Bundle();
+            stringSearch.putString(STRING_BUNDLE_INDEX, query);
+            mFragmentResult = new FragmentResultLayout();
+            mFragmentResult.setArguments(stringSearch);
+            mTransaction = getSupportFragmentManager().beginTransaction();
+            mTransaction.setCustomAnimations(R.anim.left, R.anim.right);
+            mTransaction.replace(R.id.fragment, mFragmentResult);
+            mTransaction.addToBackStack(null);
+            mTransaction.commit();
+        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            Uri uri = intent.getData();
+            Cursor cursor = managedQuery(uri, null, null, null, null);
+            cursor.moveToFirst();
+            int rIndex = cursor.getColumnIndexOrThrow(RecordsDbHelper.KEY_DATA);
+            Bundle stringSearch = new Bundle();
+            stringSearch.putString(STRING_BUNDLE_INDEX, cursor.getString(rIndex));
+            mFragmentResult = new FragmentResultLayout();
+            mFragmentResult.setArguments(stringSearch);
+            mTransaction = getSupportFragmentManager().beginTransaction();
+            mTransaction.setCustomAnimations(R.anim.left, R.anim.right);
+            mTransaction.replace(R.id.fragment, mFragmentResult);
+            mTransaction.addToBackStack(null);
+            mTransaction.commit();
+        }
+
+        /**
+         * method allows you to go back when you press
+         @Override public void onBackPressed() {
+         final Dialog alertDialog = new Dialog(Main.this);
+         alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+         final LinearLayout alertDialogCust = (LinearLayout) getLayoutInflater()
+         .inflate(R.layout.alert_dialog_main, null);
+         alertDialog.setContentView(alertDialogCust);
+         Button yes = (Button) alertDialogCust.findViewById(R.id.yes);
+         Button not = (Button) alertDialogCust.findViewById(R.id.not);
+         yes.setOnClickListener(new View.OnClickListener() {
+         @Override public void onClick(View v){
+         Main.this.finish();
+         }
+         });
+         not.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View v) {
+         alertDialog.cancel();
+         }
+         });
+         alertDialog.show();
+         return;
+         }*/
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    public boolean onQueryTextChange(String text_new) {
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        if (query == null) {
+            Toast.makeText(getApplicationContext(), "Null Query", Toast.LENGTH_SHORT).show();
+        } else {
+            saveTask(mSearchView.getQuery().toString());
+            //forming a query string and transferring it by means of Bundle
+            //Создаем экземпляр SearchRecentSuggestions
+            Bundle stringSearch = new Bundle();
+            stringSearch.putString(STRING_BUNDLE_INDEX, mSearchView.getQuery().toString());
+            mFragmentResult = new FragmentResultLayout();
+            mFragmentResult.setArguments(stringSearch);
+            mTransaction = getSupportFragmentManager().beginTransaction();
+            mTransaction.setCustomAnimations(R.anim.left, R.anim.right);
+            mTransaction.replace(R.id.fragment, mFragmentResult);
+            mTransaction.addToBackStack(null);
+            mTransaction.commit();
+        }
+
+        return true;
+    }
+
+    private void saveTask(String data) {
+        mDbHelper.createRecord(data);
+    }
+
+    /**
+     * method calls the search string
+     *
+     * @param item it contains elements of the current menu
+     * @return recursive call
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                onSearchRequested();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
+
---
+++ GitHubSearch/app/src/main/java/com/example/nitrogenium/githubsearch/RecordsDbHelper.java
@@ -0,0 +1,149 @@
+package com.example.nitrogenium.githubsearch;
+
+import java.util.HashMap;
+
+import android.app.SearchManager;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+/**
+ * class writes and reads from the database
+ */
+public class RecordsDbHelper {
+
+    //Единственный столбец в таблице - данные
+    public static final String KEY_DATA = SearchManager.SUGGEST_COLUMN_TEXT_1;
+
+    private static final String TAG = "RecordsDbHelper";
+    private DatabaseHelper mDbHelper;
+    private SQLiteDatabase mDb;
+
+    private static final String DATABASE_NAME = "datas";
+    private static final String DATABASE_TABLE = "records";
+    private static final int DATABASE_VERSION = 2;
+
+    //Сценарий создания БД
+    private static final String DATABASE_CREATE =
+            "CREATE VIRTUAL TABLE " + DATABASE_TABLE +
+                    " USING fts3 (" + KEY_DATA + ");";
+
+    private static final HashMap<String, String> mColumnMap = buildColumnMap();
+
+    /**
+     * Returns the cursor that points to a record rowId
+     *
+     * @param rowId   id the returned record
+     * @param columns columns returned records; if null, the all
+     * @return cursor pointing to a specific entry, null - if no entry is found
+     */
+    public Cursor getRecord(String rowId, String[] columns) {
+        String selection = "rowid = ?";
+        String[] selectionArgs = new String[]{rowId};
+
+        return query(selection, selectionArgs, columns);
+    }
+
+    /**
+     * returns the cursor that points to all the records that match the query
+     *
+     * @param query   text search query
+     * @param columns columns returned records; if null, the all
+     * @return cursor indicating the records that match the query, null - if no record is found
+     */
+    public Cursor getRecordMatches(String query, String[] columns) {
+        String selection = KEY_DATA + " MATCH ?";
+        String[] selectionArgs = new String[]{query + "*"};
+
+        return query(selection, selectionArgs, columns);
+    }
+
+    /**
+     * It creates a display of all kinds of requested columns.
+     * To be set as a projection in SQLiteQueryBuilder.
+     * It is necessary to assign each entry a unique value SUGGEST_COLUMN_INTENT_DATA_ID
+     * Are used to obtain specific recording URI.
+     */
+    private static HashMap<String, String> buildColumnMap() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put(KEY_DATA, KEY_DATA);
+        map.put(BaseColumns._ID, "rowid AS " +
+                BaseColumns._ID);
+        map.put(SearchManager.SUGGEST_COLUMN_INTENT_DATA_ID, "rowid AS " +
+                SearchManager.SUGGEST_COLUMN_INTENT_DATA_ID);
+        return map;
+    }
+
+    /**
+     * @param selection     operator sampling
+     * @param selectionArgs arguments replace "?" the query to the database
+     * @param columns       columns returned record
+     * @return cursor pointing to all records that match the search query
+     */
+    private Cursor query(String selection, String[] selectionArgs, String[] columns) {
+        /* SQLiteBuilder provides an opportunity to create a mapping for all
+          * Required database column that allows you to not report a content provider
+          * Real names columns.
+         */
+
+        SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        builder.setTables(DATABASE_TABLE);
+        builder.setProjectionMap(mColumnMap);
+
+        Cursor cursor = builder.query(mDbHelper.getReadableDatabase(),
+                columns, selection, selectionArgs, null, null, null);
+        if (cursor == null) {
+            return null;
+        } else if (!cursor.moveToFirst()) {
+            cursor.close();
+            return null;
+        }
+        return cursor;
+    }
+
+    /**
+     * Create / open database
+     */
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS records");
+            onCreate(db);
+        }
+    }
+
+    public RecordsDbHelper(Context context) {
+        mDbHelper = new DatabaseHelper(context);
+    }
+
+    /**
+     * Adds an entry to the table
+     *
+     * @param data data stored in the table
+     * @return id entry, or -1 if the addition failed
+     */
+    public long createRecord(String data) {
+        mDb = mDbHelper.getWritableDatabase();
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_DATA, data);
+        return mDb.insert(DATABASE_TABLE, null, initialValues);
+    }
+
+}
